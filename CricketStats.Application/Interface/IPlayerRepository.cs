ed3e63e6ddcc0a43a9221de00031a1808e6d68fc
dell <official.sysinfo@gmail.com>
2025-12-07 17:17:53 +0530

    added Players Get,Post,GetAll,GetById minimal API in program.cs

---
+++ CricketStats.Application/Interface/IPlayerRepository.cs
@@ -0,0 +1,18 @@
+﻿using CricketStats.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CricketStats.Application.Interface
+{
+    public interface IPlayerRepository
+    {
+        List<Player> GetAll();
+        Player? GetById(int id);
+        Player Add(Player player);
+        Player? Update(Player player);
+        bool Delete(int id);
+    }
+}
---
+++ CricketStats.Infrastructure/Service/InMemoryPlayerRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CricketStats.Infrastructure.Service
+{
+    internal class InMemoryPlayerRepository
+    {
+    }
+}
